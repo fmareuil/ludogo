20c6ee9ff97b46fa5c2867ff147dc151dfcf48d8
fmareuil <fmareuil@pasteur.fr>
2019-06-07 23:28:54 +0200

    if not plot outline get plot

--- film/views.py
+++ film/views.py
@@ -46,6 +46,8 @@ class MovieCreateView(generic.CreateView):
                 newmovie['synopsis'] = movie['synopsis'][0]
             elif 'plot outline' in movie:
                 newmovie['synopsis'] = movie['plot outline']
+            elif 'plot' in movie:
+                newmovie['synopsis'] = movie['plot']
             if 'title' in movie:
                 newmovie['title'] = movie['title']
             if 'year' in movie:
